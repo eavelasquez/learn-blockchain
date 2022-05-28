d3fe48a1893d462f5e02fb4798e7e113bc1ff6a4
Esteban Velasquez <eavc.30@gmail.com>
2022-05-27 21:51:16 -0500

    feat: add validator module file
    - add function to validate blockchain.

---
+++ src/blockchain/modules/validate.js
@@ -0,0 +1,24 @@
+import Block from '../block.js';
+
+export default (blockchain) => {
+  const [genesis, ...blocks] = blockchain;
+
+  if (JSON.stringify(genesis) !== JSON.stringify(Block.genesis)) {
+    throw new Error('The genesis block is invalid');
+  }
+
+  for (let i = 0; i < blocks.length; i += 1) {
+    const { data, hash, previousHash, timestamp } = blocks[i];
+    const previousBlock = blockchain[i];
+
+    if (previousHash !== previousBlock.hash) {
+      throw new Error('The previous hash is invalid');
+    }
+
+    if (hash !== Block.hash(data, previousHash, timestamp)) {
+      throw new Error('The hash is invalid');
+    }
+  }
+
+  return true;
+};
