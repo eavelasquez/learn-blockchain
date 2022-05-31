331b050d304f95dfe61381e8fac5581c7a5c4f1f
Esteban Velasquez <eavc.30@gmail.com>
2022-05-31 07:14:20 -0500

    feat: update p2p file
    - add broadcast method.
    - modify method onConnection for the transmission of messages (chain).

--- src/service/p2p.js
+++ src/service/p2p.js
@@ -2,6 +2,7 @@ import WebSocket, { WebSocketServer } from 'ws';
 
 const { P2P_PORT: p2pPort = 5000, PEERS: peersToConnect } = process.env;
 const peers = peersToConnect ? peersToConnect.split(',') : [];
+const MESSAGE_TYPES = { chain: 'CHAIN' };
 
 class P2PService {
   constructor(blockchain) {
@@ -10,8 +11,25 @@ class P2PService {
   }
 
   onConnection(socket) {
-    this.sockets.push(socket);
+    const { blockchain: { chain } } = this;
+
     console.log('[ws:socket] connected');
+    this.sockets.push(socket);
+
+    socket.on('message', (message) => {
+      const { type, value } = JSON.parse(message);
+      console.group('[ws:message]');
+      console.log(`type: ${type} | value:`);
+      console.log(value);
+      console.groupEnd();
+    });
+
+    socket.send(
+      JSON.stringify({
+        type: MESSAGE_TYPES.chain,
+        value: chain,
+      })
+    );
   }
 
   listen() {
@@ -24,6 +42,13 @@ class P2PService {
     });
 
     console.log(`Listening for peer-to-peer connections on: ${p2pPort}`);
+  }
+
+  broadcast(type, value) {
+    console.log(`[ws:broadcast] type: ${type}...`);
+
+    const message = JSON.stringify({ type, value });
+    this.sockets.forEach((socket) => socket.send(message));
   }
 }
 
