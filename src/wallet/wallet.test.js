8e24d7638463b56b10e0b765409d3f021ed9cbce
Esteban Velasquez <eavc.30@gmail.com>
2022-06-06 09:20:05 -0500

    test: add wallet.test.js file with tests cases

---
+++ src/wallet/wallet.test.js
@@ -0,0 +1,23 @@
+import Wallet, { INITIAL_BALANCE } from './wallet';
+
+describe('Wallet class', () => {
+  let wallet;
+
+  beforeEach(() => {
+    wallet = new Wallet();
+  });
+
+  it('should create a healthy wallet', () => {
+    expect(wallet.balance).toEqual(INITIAL_BALANCE);
+    expect(typeof wallet.keyPair).toEqual('object');
+    expect(typeof wallet.publicKey).toEqual('string');
+    expect(wallet.publicKey.length).toEqual(130);
+  });
+
+  it('should return a string representation of the wallet', () => {
+    const result = wallet.toString();
+
+    expect(result).toMatch(/Wallet -/);
+    expect(typeof result).toEqual('string');
+  });
+});
