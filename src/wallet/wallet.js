fa3bb7b56179d9a14b669f230109148c16c0e382
Esteban Velasquez <eavc.30@gmail.com>
2022-06-07 08:35:15 -0500

    feat: update Wallet class
    - receive a blockchain as an argument in the constructor.
    - add method to create a transaction.

--- src/wallet/wallet.js
+++ src/wallet/wallet.js
@@ -1,10 +1,12 @@
 import { elliptic, generateHash } from '../modules/index.js';
+import Transaction from './transaction';
 
 const INITIAL_BALANCE = 100;
 
 class Wallet {
-  constructor() {
+  constructor(blockchain) {
     this.balance = INITIAL_BALANCE;
+    this.blockchain = blockchain;
     this.keyPair = elliptic.createKeyPair();
     this.publicKey = this.keyPair.getPublic().encode('hex');
   }
@@ -19,6 +21,22 @@ class Wallet {
       balance:   ${this.balance}
       publicKey: ${this.publicKey}
     `;
+  }
+
+  createTransaction({ recipientAddress, amount }) {
+    if (amount > this.balance) {
+      throw new Error(`Amount: ${amount} exceeds balance: ${this.balance}`);
+    }
+
+    let txn = this.blockchain.memoryPool.findTransaction(this.publicKey);
+    if (txn) {
+      txn.update(this, recipientAddress, amount);
+    } else {
+      txn = Transaction.create(this, recipientAddress, amount);
+      this.blockchain.memoryPool.addOrUpdateTransaction(txn);
+    }
+
+    return txn;
   }
 }
 
