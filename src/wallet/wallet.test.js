fd0462fdffecc850e74c63362b4bf5c024d5759e
Esteban Velasquez <eavc.30@gmail.com>
2022-06-07 08:36:48 -0500

    test: update wallet.test.js file
    - update wallet instance to receive a blockchain.
    - create test cases to create a transaction.

--- src/wallet/wallet.test.js
+++ src/wallet/wallet.test.js
@@ -1,10 +1,13 @@
+import Blockchain from '../blockchain';
 import Wallet, { INITIAL_BALANCE } from './wallet';
 
 describe('Wallet class', () => {
   let wallet;
+  let blockchain;
 
   beforeEach(() => {
-    wallet = new Wallet();
+    blockchain = new Blockchain();
+    wallet = new Wallet(blockchain);
   });
 
   it('should create a healthy wallet', () => {
@@ -26,5 +29,39 @@ describe('Wallet class', () => {
 
     expect(typeof signature).toEqual('object');
     expect(signature).toEqual(wallet.sign('foo'));
+  });
+
+  describe('creating a transaction', () => {
+    let transaction;
+    let recipientAddress;
+    let amount;
+
+    beforeEach(() => {
+      amount = 5;
+      recipientAddress = 'random-address';
+      transaction = wallet.createTransaction({ recipientAddress, amount });
+    });
+
+    describe('and doing the same transaction', () => {
+      beforeEach(() => {
+        transaction = wallet.createTransaction({ recipientAddress, amount });
+      });
+
+      it('should double the amount subtracted from the wallet account', () => {
+        const output = transaction.outputs.find(
+          (output) => output.address === wallet.publicKey
+        );
+
+        expect(output.amount).toEqual(wallet.balance - amount * 2);
+      });
+
+      it('should clone the `amount` output for the transaction', () => {
+        const amounts = transaction.outputs
+          .filter((output) => output.address === recipientAddress)
+          .map((output) => output.amount);
+
+        expect(amounts).toEqual([amount, amount]);
+      });
+    });
   });
 });
