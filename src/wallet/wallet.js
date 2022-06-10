25fd2ca7d0f548ef6a755e18513b351cf69969ee
Esteban Velasquez <eavc.30@gmail.com>
2022-06-09 20:31:45 -0500

    feat: add calculateBalance method in wallet class

--- src/wallet/wallet.js
+++ src/wallet/wallet.js
@@ -38,6 +38,50 @@ class Wallet {
 
     return txn;
   }
+
+  calculateBalance() {
+    const {
+      blockchain: { chain = [] },
+      publicKey,
+    } = this;
+    let { balance = 0 } = this;
+    const transactions = [];
+
+    chain.forEach(({ data = [] }) => {
+      if (Array.isArray(data)) {
+        data.forEach((txn) => transactions.push(txn));
+      }
+    });
+
+    const walletInputTxs = transactions.filter(
+      (txn) => txn.input.address === publicKey,
+    );
+    let timestamp = 0;
+
+    if (walletInputTxs.length > 0) {
+      const recentInputTx = walletInputTxs
+        .sort((a, b) => a.input.timestamp - b.input.timestamp)
+        .pop(); // get the most recent input transaction
+
+      timestamp = recentInputTx.input.timestamp;
+      balance = recentInputTx.outputs.find(
+        ({ address }) => address === publicKey,
+      ).amount;
+    }
+
+    transactions
+      .filter(({ input }) => input.timestamp > timestamp)
+      .forEach(({ outputs }) => {
+        outputs.find(({ address, amount }) => {
+          if (address === publicKey) {
+            balance += amount;
+          }
+          return false;
+        });
+      });
+
+    return balance;
+  }
 }
 
 export { INITIAL_BALANCE };
